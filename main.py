a1ff8a764f06f813f9b0117c124d89b4430a033d
Javier Fuentes Mora <javier.fuentesm@hotmail.com>
2022-11-11 23:22:59 -0500

    se termina , decarga video, transcribe y genera el md para logseq con timestamps

--- main.py
+++ main.py
@@ -2,10 +2,7 @@ import time
 import os
 import pytube
 import whisper
-
 from typing import Iterator, TextIO
-
-# from whisper.utils import write_srt
 
 
 def srt_format_timestamp(seconds: float):
@@ -24,14 +21,7 @@ def srt_format_timestamp(seconds: float):
     return f"{hours}:" + f"{minutes:02d}:{seconds:02d},{milliseconds:03d}"
 
 
-# write a function for from str timestamp to youtube timestamp
-def str_to_youtube_timestamp(time_str):
-    time_str = time_str.split(':')
-    time_str = [int(i) for i in time_str]
-    return time_str[0]*60*60 + time_str[1]*60 + time_str[2]
-
-
-def write_srt(transcript: Iterator[dict], file: TextIO):
+def write_md(transcript: Iterator[dict], file: TextIO):
     for segment in transcript:
         print(
             f"- [ {{{{youtube-timestamp {(srt_format_timestamp(segment['start']))}}}}} --> {{{{youtube-timestamp {srt_format_timestamp(segment['end'])}}}}} ]   "
@@ -43,14 +33,12 @@ def write_srt(transcript: Iterator[dict], file: TextIO):
 
 video = 'https://www.youtube.com/watch?v=Og-ah5UIeHY&t=1385'
 data = pytube.YouTube(video)
+
 # get the name of the video
 video_name = data.title
 
 # Converting and downloading as 'MP4' file
-# audio = data.streams.get_audio_only()
-# save the file with the time stamp
 name = video_name + str(time.time())
-# audio.download(filename=name)
 
 # download video
 video = data.streams.get_highest_resolution()
@@ -59,14 +47,9 @@ video.download(filename=name + '.mp4')
 model = whisper.load_model("base")
 text = model.transcribe(name + '.mp4', fp16=False, verbose=True)
 
-# save str to file
-# save VTT
+# save md file
 with open(name + '.md', 'w') as srt:
-    write_srt(text["segments"], file=srt)
-
-# create a text file and write the output
-# with open(name + '.txt', 'w') as f:
-#     f.write(text['segments'][0]['text'])
+    write_md(text["segments"], file=srt)
 
 # delete the  file
-os.remove(name+'.mp4')
+os.remove(name + '.mp4')
